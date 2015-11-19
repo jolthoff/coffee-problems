f58811f7edfc631297eff10dadd4396f1358f409
Jordan Olthoff <jordanolthoff@Jordans-MacBook-Pro.local>
2015-11-18 18:55:12 -0800

    Implement singleNumber

---
+++ singleNumber.js
@@ -0,0 +1,15 @@
+var singleNumber = function(nums) {
+    var found = {};
+    var unique = [];
+    for (var i = 0; i < nums.length; i++) {
+        found[nums[i]] = found[nums[i]] + 1 || 1;
+    }
+
+    for (var key in found) {
+        if (found[key] === 1) {
+            unique.push(Number(key));
+        }
+    }
+
+    return unique;
+};
